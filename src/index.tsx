57e2eb6a458375011a70bc872e91db4df6561fbe
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-10 16:01:51 +0100

    add css folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import './stylesheets/application.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
