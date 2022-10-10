259f7df883f9853aa5e78e026c6aeda816eddefe
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-10 13:34:06 +0100

    add basic card bootstrap

--- src/Dogs/Dog.tsx
+++ src/Dogs/Dog.tsx
@@ -2,17 +2,21 @@ import { IDog } from '../types/data'
 
 const Dog = (props: IDog): React.ReactElement | null  => {
   return(
-    <div>
-      <h2>{props.breed}</h2>
-      <img src={`https://cdn2.thedogapi.com/images/${props.reference_image_id}.jpg`} alt="" />
-      <ul>
-        <li>Ideal weight range: {props.weight_metric}lbs | {props.weight_metric}kg</li>
-        <li>Ideal height range: {props.height_metric}lbs | {props.height_metric}kg</li>
-        <li>Breed group: {props.breed_group}</li>
-        <li>Role: {props.bred_for}</li>
-        <li>Average lifespan: {props.life_span}</li>
-        <li>Temperament: {props.temperament}</li>
-      </ul>
+    <div className="col">
+      <div className='card'>
+        <img src={`https://cdn2.thedogapi.com/images/${props.reference_image_id}.jpg`} alt="" className='card-img-top'/>
+        <div className="card-body">
+          <h2 className='card-title'>{props.breed}</h2>
+          <ul>
+            <li className='card-text'>Ideal weight range: {props.weight_metric}lbs | {props.weight_metric}kg</li>
+            <li className='card-text'>Ideal height range: {props.height_metric}lbs | {props.height_metric}kg</li>
+            <li className='card-text'>Breed group: {props.breed_group}</li>
+            <li className='card-text'>Role: {props.bred_for}</li>
+            <li className='card-text'>Average lifespan: {props.life_span}</li>
+            <li className='card-text'>Temperament: {props.temperament}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Dogs/DogList.tsx
+++ src/Dogs/DogList.tsx
@@ -26,21 +26,23 @@ const DogList = () => {
   return(
     <div>
       <h1>Browse all dogs</h1>
-      {dogs.map((dog:IDog) => (
-        <Dog
-          key={dog.id}
-          breed={dog.breed}
-          weight_imperial={dog.weight_imperial}
-          weight_metric={dog.weight_metric}
-          height_imperial={dog.height_imperial}
-          height_metric={dog.height_metric}
-          bred_for={dog.bred_for}
-          breed_group={dog.breed_group}
-          life_span={dog.life_span}
-          temperament={dog.temperament}
-          reference_image_id={dog.reference_image_id}
-        />
-      ))}
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+        {dogs.map((dog:IDog) => (
+          <Dog
+            key={dog.id}
+            breed={dog.breed}
+            weight_imperial={dog.weight_imperial}
+            weight_metric={dog.weight_metric}
+            height_imperial={dog.height_imperial}
+            height_metric={dog.height_metric}
+            bred_for={dog.bred_for}
+            breed_group={dog.breed_group}
+            life_span={dog.life_span}
+            temperament={dog.temperament}
+            reference_image_id={dog.reference_image_id}
+            />
+        ))}
+      </div>
     </div>
   )
 }
