428e2e3dfb11971b6bb8b45dfa374dfcd97ee135
Aaron Lambley <aaron.lambley@gmail.com>
2022-10-10 12:21:23 +0100

    create page templates

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return(
+    <div>
+      <h1>About this app</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat aperiam culpa, possimus explicabo voluptatibus placeat saepe. Assumenda a similique eos illum quos hic officia debitis consequatur explicabo. Unde, reiciendis omnis.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <h1>Find out your dog's ideal weight</h1>
+  )
+}
+
+export default Home
